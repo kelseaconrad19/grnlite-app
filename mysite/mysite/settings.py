f5fa595b1de8787aabd6d2ed29a38ec2f537c932
JSander72 <jamesearlsanders2@outlook.com>
2024-11-29 16:44:02 -0500

    PostgreSQL download

---
+++ mysite/mysite/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grnlite_db', 
+        'USER': 'grnlite_user',
+        'PASSWORD': '12345',
+        'HOST': 'localhost', 
+        'PORT': '5432', 
+    }
+}
